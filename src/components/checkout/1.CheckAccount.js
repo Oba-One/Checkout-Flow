7b39fbef0b67e66cb92bd37f42b5a9f6deff6178
Oba-One <35589781+Oba-One@users.noreply.github.com>
2019-01-10 12:32:42 -0800

    created components for checkout page/flow

---
+++ src/components/checkout/1.CheckAccount.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CheckAccount = () => (
+    <div></div>
+);
+
+export default CheckAccount;
---
+++ src/components/checkout/5.ReviewOrder.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ReviewOrder = () => (
+    <div></div>
+);
+
+export default ReviewOrder;
