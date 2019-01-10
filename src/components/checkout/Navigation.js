93e6e31cc6de5b5febbdad501b71d72e41b9f25b
Oba-One <35589781+Oba-One@users.noreply.github.com>
2019-01-10 12:33:22 -0800

    created checkout navigation element

---
+++ src/components/checkout/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Header = styled.div`
+
+`;
+
+const Navigation = () => (
+    <div></div>
+);
+
+export default Navigation;
