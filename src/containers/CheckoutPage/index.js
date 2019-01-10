ebe1667abff4208ac0d5e95e5596ceceedddf6e7
Oba-One <35589781+Oba-One@users.noreply.github.com>
2019-01-10 14:02:42 -0800

    imported context api and updated routes

--- src/containers/CheckoutPage/index.js
+++ src/containers/CheckoutPage/index.js
@@ -1,6 +1,9 @@
+// Container for Holding Checkout Routes and Consuming Context API
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
 import styled from 'styled-components';
+
+import { CheckoutContext } from '../../containers/CheckoutProvider/context'
 
 import Navigation from '../../components/checkout/Navigation'
 import CheckAccount from '../../components/checkout/1.CheckAccount';
@@ -18,19 +21,23 @@ const Container = styled.section`
     height: 100vh;
 `;
 
-
+// Holds all routes and context consumer to pass down state to checkout components
 const CheckoutPage = () => (
-    <Container>
-        <Navigation/>
-        <Switch>
-            <Route path="/" component={CheckAccount}/>
-            <Route path="/" component={CreateAccountOrShipping}/>
-            <Route path="/" component={ShippingDetails}/>
-            <Route path="/" component={PaymentDetails}/>
-            <Route path="/" component={ReviewOrder}/>
-            <Route path="/" component={OrderConfirmation}/>
-        </Switch>
-    </Container>
+    <CheckoutContext.Consumer>
+        { checkout => (
+        <Container>
+            <Navigation/>
+            <Switch>
+                <Route exact path="/" component={CheckAccount}/>
+                <Route path="/1" component={CreateAccountOrShipping}/>
+                <Route path="/2" component={ShippingDetails}/>
+                <Route path="/3" component={PaymentDetails}/>
+                <Route path="/4" component={ReviewOrder}/>
+                <Route path="/5" component={OrderConfirmation}/>
+            </Switch>
+        </Container>    
+        )}
+    </CheckoutContext.Consumer>
 );
 
 export default CheckoutPage;
